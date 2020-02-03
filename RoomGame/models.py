cf13a4ad514249ea86a245f7a319f16a1e598048
Tyler <tylerwesleyquinn@gmail.com>
2020-02-02 19:53:08 -0500

    Player model migrations

--- RoomGame/models.py
+++ RoomGame/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from uuid import uuid4
 
 class Player(models.Model):
-    user_name = models.CharField(max_length=30, unique=true)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    user_name = models.CharField(max_length=30, unique=True)
     user_pass = models.CharField(max_length=30)
     current_room = models.CharField(max_length=30)
     score = models.IntegerField(default=0)
     player_avatar = models.IntegerField(default=0)
-    

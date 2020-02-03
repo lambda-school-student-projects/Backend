290426e09b9ad22c5bba9acaebfc553e77ebe947
Tyler <tylerwesleyquinn@gmail.com>
2020-02-02 22:20:51 -0500

    Creates superuser / boiler plate for django RESTful API

--- CSBuild/settings.py
+++ CSBuild/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'RoomGame',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
--- RoomGame/models.py
+++ RoomGame/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from uuid import uuid4
 
+#if changing the model -
+#https://github.com/LambdaSchool/Intro-Django/blob/master/guides/day2.md#migrations-with-new-fields
 class Player(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     user_name = models.CharField(max_length=30, unique=True)
@@ -8,3 +10,4 @@ class Player(models.Model):
     current_room = models.CharField(max_length=30)
     score = models.IntegerField(default=0)
     player_avatar = models.IntegerField(default=0)
+

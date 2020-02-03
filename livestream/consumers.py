27a0ea571c41f4a7a0c7f26672218099353c2f2b
Michael Redig <mredig@gmail.com>
2020-02-03 16:39:40 -0600

    super simple and bad game loop

--- livestream/consumers.py
+++ livestream/consumers.py
@@ -1,11 +1,18 @@
 from channels.generic.websocket import WebsocketConsumer
 import json
+import time
 
 
 class RoomConsumer(WebsocketConsumer):
+    def __init__(self, secondConsumerParameter):
+        super().__init__(secondConsumerParameter)
+        print("")
+        print("NEW consumer started up!")
+
     def connect(self):
         print("New connection")
         self.accept()
+        # self.gameLoop()
 
     def disconnect(self, close_code):
         print("disconnected")
@@ -21,4 +28,15 @@ class RoomConsumer(WebsocketConsumer):
         self.send(text_data=json.dumps({'message': message}))
 
 
-    
+    def gameLoop(self):
+        alpha = list("abcdefghijklmnopqrstuvexyz")
+        counter = 0
+        totalCount = 0
+        while True:
+            if counter == len(alpha):
+                counter = 0
+            self.send(text_data=json.dumps({'message':f"{alpha[counter]} {totalCount}"}))
+            counter += 1
+            totalCount += 1
+            print(totalCount)
+            time.sleep(0.05)

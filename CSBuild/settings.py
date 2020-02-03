5b1ff7581d7f4159467590f81cca518594853b8c
Tyler <tylerwesleyquinn@gmail.com>
2020-02-03 01:05:02 -0500

    File structure rename - RoomGame:Rooms

--- CSBuild/settings.py
+++ CSBuild/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'RoomGame',
+    'Rooms',
     'rest_framework',
 ]
 
--- CSBuild/urls.py
+++ CSBuild/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('RoomGame.urls')),
+    path('api/', include('Rooms.urls')),
 ]

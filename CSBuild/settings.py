ef2205b77965e5b54633cce62be6aa27cd9c0146
Michael Redig <mredig@gmail.com>
2020-02-09 15:20:16 -0600

    (update) added dev subdomain for hosting from

--- CSBuild/settings.py
+++ CSBuild/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]',
-                 'bsvbattleroyale.herokuapp.com', 'bsvbattle.redig.me', 'imac.nl.redig.me']
+                 'dev.bsvbattle.redig.me', 'bsvbattle.redig.me', 'imac.nl.redig.me']
 
 SITE_ID = 1
 
